48ba4b4964b321ddd2600eab832cf7298bdaac15
greg5320 <art3m43@gmail.com>
2022-11-21 14:45:32 +0300

    Laba8 completed

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@ int factorial(int x) {
 }
 
 double **squarematrix(int n, float x) {
-//    double arr[n][n];
     double **arr = new double *[n];
     for (int i = 0; i < n; i++) {
         arr[i] = new double[n];
@@ -21,12 +20,11 @@ double **squarematrix(int n, float x) {
         for (int j = 0; j < n; j++) {
             if ((i > j) || (i < j)) {
                 if (i > j)
-                    x = pow(x, i) / (pow(factorial(j), i));
+                    arr[i][j] = pow(x, i) / (pow(factorial(j), i));
                 else if (i < j)
-                    x = pow(-x, i) / (pow(factorial(j), i));
+                    arr[i][j] = pow(-x, i) / (pow(factorial(j), i));
             } else
-                x = 1;
-            arr[i][j] = x;
+                arr[i][j] = 1;
         }
     }
     return arr;
@@ -65,10 +63,18 @@ double **matrixB() {
     return arr;
 }
 
-void printmatrix(double **arr, int n, int m) {
+void printmatrixA(double **arr, int n, int m) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++)
-            cout << fixed << setprecision(8) << scientific << arr[i][j] << " ";
+            cout << fixed << setw(16) << setprecision(8) << scientific << arr[i][j] << " ";
+        cout << endl;
+    }
+}
+
+void printmatrixB(double **arr, int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++)
+            cout << fixed << setw(4) << setprecision(0) << arr[i][j] << " ";
         cout << endl;
     }
 }
@@ -80,12 +86,18 @@ int main() {
     cin >> n;
     cout << "Введите значение x " << endl;
     cin >> x;
-    double **tempA = squarematrix(n, x);
-    printmatrix(tempA, n, n);
+    double **A = squarematrix(n, x);
+    printmatrixA(A, n, n);
     cout << endl;
-    double **tempB = matrixB();
-    printmatrix(tempB, 10, 10);
-    delete[]tempA;
-    delete[]tempB;
+    double **B = matrixB();
+    printmatrixB(B, 10, 10);
+    cout << endl;
+    delete[]A;
+    cout << B << "  " << B[0] << "  " << B[2] << endl;
+    cout << B[0][0] << "  " << **B << "  " << *B[0] << endl; //
+    cout << *(*(B + 1)) << "  " << *B[1] << endl;
+    cout << *(B[0] + 1) << "  " << *(*B + 1) << endl;
+    cout << B[0][20] << "  " << *(B[0] + 20) << "  " << *B[2] << endl;
+    delete[]B;
     return 0;
 }
